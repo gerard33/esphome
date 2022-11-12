7cdd7e64cc25ab09f773059693eceb7191417532
Gerard <gerard33@users.noreply.github.com>
2022-11-12 22:56:49 +0100

    Update __init__.py

--- esphome/components/ethernet/__init__.py
+++ esphome/components/ethernet/__init__.py
@@ -89,7 +89,6 @@ CONFIG_SCHEMA = cv.All(
         }
     ).extend(cv.COMPONENT_SCHEMA),
     _validate,
-    cv.only_with_arduino,
 )
 
 
@@ -124,6 +123,3 @@ async def to_code(config):
         cg.add(var.set_manual_ip(manual_ip(config[CONF_MANUAL_IP])))
 
     cg.add_define("USE_ETHERNET")
-
-    if CORE.is_esp32:
-        cg.add_library("WiFi", None)
